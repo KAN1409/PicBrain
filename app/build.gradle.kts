e978b4f21711b459737d52eeb99da7d3bde89e60
KAN1409 <engkareemnasser@gmail.com>
2026-09-10 14:29:25 +0300

    PicBrain v0.0.19: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kareem.picbrain"
         minSdk = 29
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.0.18"
+        versionCode = 19
+        versionName = "0.0.19"
     }
 
     compileOptions {
